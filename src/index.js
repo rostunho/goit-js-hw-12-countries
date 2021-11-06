57172639e3f79726d0b2b491386a94f7929b8da3
Rostyk Podolets <ros.podolets@gmail.com>
2021-11-06 11:05:55 +0200

    v 0.1
    Fetched first data.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './sass/main.scss';
+
+import coutryCardTmp from './templates/couuntry-card.hbs';
+
+const refs = {
+  cardContainer: document.querySelector('.card-container'),
+};
+
+fetch('https://restcountries.com/v2/name/belgium')
+  .then(response => {
+    return response.json();
+  })
+  .then(countries => {
+    console.log(countries);
+    const markup = coutryCardTmp(countries);
+    console.log(markup);
+    refs.cardContainer.innerHTML = markup;
+  })
+  .catch(error => {
+    console.log(error);
+  });
