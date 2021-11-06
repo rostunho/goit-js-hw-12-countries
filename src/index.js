e3ef6b5785f6c45f3a8081e795605801b2097bbf
Rostyk Podolets <ros.podolets@gmail.com>
2021-11-06 21:06:21 +0200

    v 0.6
    After debouncing.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import './sass/main.scss';
 import fetchCountries from './js/fetch-countries';
 import renderCountryCard from './js/render-markup';
+import onFetchError from './js/error';
 import refs from './js/refs';
+import debounce from 'lodash.debounce';
 
-refs.searchForm.addEventListener('input', onSearch);
+refs.searchForm.addEventListener('input', debounce(onSearch, 500));
 
 function onSearch(event) {
   event.preventDefault();
-  const searchQuery = event.currentTarget.value;
+  const searchQuery = event.target.value;
   fetchCountries(searchQuery).then(renderCountryCard).catch(onFetchError);
 }
